a4b6e7d38153a41aee5d13f6b567c538c971e14b
刘亚军 <liuyajun1999@icloud.com>
2022-03-15 20:47:54 +0800

    优化

--- YJTaskMark/Classes/Util/YJSpeechManager.m
+++ YJTaskMark/Classes/Util/YJSpeechManager.m
@@ -123,7 +123,7 @@ static CGFloat kSoundOffset = 8;
     [SSOralEvaluatingManager shareManager].delegate = self;
     
     [SSOralEvaluatingManager registerEvaluatingManagerConfig:managerConfig];
-    [[SSOralEvaluatingManager shareManager] registerEvaluatingType:OralEvaluatingTypeLine];
+    [[SSOralEvaluatingManager shareManager] registerEvaluatingType:OralEvaluatingTypeLine userId:[YJNetManager defaultManager].userID];
     
 }
 /**
